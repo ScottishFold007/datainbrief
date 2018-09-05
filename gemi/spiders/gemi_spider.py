8e2a8690f28c45d06f77585fc044e2aaae3fa860
Selim <ozturkselim00@gmail.com>
2018-09-05 16:25:54 +0300

    Status: UP

--- gemi/spiders/gemi_spider.py
+++ gemi/spiders/gemi_spider.py
@@ -16,13 +16,18 @@ class GemiSpider(scrapy.Spider):
             'gemi.pipelines.MongoPipeline': 300,  # pipeline with smaller number executed first
         }
     }
+
     # entry point
-    def __init__(self, next_page=False, details=False *args, **kwargs):
+    def __init__(self, next_page=False, details=False, daily_search=False, *args, **kwargs):
         super(GemiSpider, self).__init__(*args, **kwargs)
         self.base_url = 'https://www.yachtworld.com'
-        self.urls = GemiUtil.generate_base_query_url()
-        self.next_page = next_page
-        self.should_get_details = details
+        self.base_search_query_url = 'https://www.yachtworld.com/core/listing/cache/searchResults.jsp'
+
+        self.urls = self.generate_base_query_url()  
+
+        self.next_page = next_page # follow to the next pages 
+        self.should_get_details = details # parse details page
+        self.daily_search = daily_search # search recent day only 
 
 
     def start_requests(self):
@@ -98,7 +103,7 @@ class GemiSpider(scrapy.Spider):
                 # send a request to the details page
                 yield scrapy.Request(
                     link, 
-                    callback = self.parseCoordinates,
+                    callback = self.parseDetails,
                     meta = {
                             'model': model,
                             'year': year,
@@ -122,6 +127,92 @@ class GemiSpider(scrapy.Spider):
                 }
             
 
+    def parseDetails(self, response):
+        detail_selector = 'div.boatdetails::text'
+        full_spec_selector = 'div.fullspecs::text'
+
+        # parse fields
+        details = page.css(detail_selector).extract()
+        fullspecs = page.css(full_spec_selector).extract()
+
+        # add field to dict
+        response.meta['fullspecs'] = fullspecs
+        response.meta['details'] = details
+
+        # send the item info to the pipeline
+        yield response.meta
+
+        
+
+
+    def generate_base_query_url(self):
+
+        # default query
+        fromLength = 25
+        fromYear = 1995
+        fromPrice = 20000
+        toPrice = 8000000
+        luom = 126  # units feet, meter=127
+        currencyid = 100  # US dollar
+
+
+
+        self.base_query = {
+                        'fromLength': fromLength,
+                        'fromYear': fromYear,
+                        'fromPrice': fromPrice,
+                        'toPrice': toPrice,
+                        'luom': luom,  # unit id
+                        'currencyId': currencyid,
+                        'ps': 50  # entries per page
+                    }
+
+        if self.daily_search:
+            base_query['pbsint'] = 1535580789155 # 1 day
+
+        base_query_url = urlencode(OrderedDict(data=self.base_search_query_url, search=base_query))
+
+        return list(base_query_url)
+
+
+    def add_extra_query_parameters(self):
+        # initialize
+        search_urls = list()
+
+
+        # dynamic query parameters
+        hull_materials = {'aluminium': 100, 'composite': 101, 'fiberglass': 102,
+                          'steel': 103, 'wood': 104, 'other': 105, 'hypalon': 106,
+                          'pvc': 107, 'ferro-cement': 108, 'carbon-fiber': 110}
+        fuels = {'gas': 100, 'diesel': 101, 'other': 102}
+        number_of_engines = {1: 100, 2: 101, 'other': 102, 'none': 103}
+        is_new = [True, False]
+        within_x_days = {1: 1535580789155, 3: 1535407989155, 7: 1535062389155, 14: 1534457589155, 30: 1533075189155,
+                    60: 1530483189155}
+
+
+        # generate dynamic queries
+        for recent_day, new_or_used, material, fuel, engine_number in product(
+                within_x_days, is_new, hull_materials,
+                fuels, number_of_engines):
+
+            extra_query_parameters = {  'is': new_or_used,
+                            'hmid': material,
+                            'ftid': fuel,
+                            'enid': engine_number,
+                            'pbsint': recent_day
+                            }
+
+            # add new parameters to the search url 
+            search_url = urlencode(OrderedDict(data=self.base_query_url, search=extra_query_parameters))
+
+            search_urls.append(search_url)
+
+        return search_urls
+
+
+
+
 
 
 
@@ -136,76 +227,4 @@ class GemiUtil(object):
                 prices.pop(i)
         return prices
 
-    @staticmethod
-    def generate_base_query_url():
-        base_url = 'https://www.yachtworld.com/core/listing/cache/searchResults.jsp'
 
-        # default query
-        fromLength = 25
-        fromYear = 1995
-        fromPrice = 20000
-        toPrice = 8000000
-        luom = 126  # units feet, meter=127
-        currencyid = 100  # US dollar
-
-        base_query = {
-                        'fromLength': fromLength,
-                        'fromYear': fromYear,
-                        'fromPrice': fromPrice,
-                        'toPrice': toPrice,
-                        'luom': luom,  # unit id
-                        'currencyId': currencyid,
-                        'ps': 100  # entries per page
-
-                    }
-
-        base_query_url = urlencode(OrderedDict(data=base_url, search=base_query))
-
-        return list(base_query_url)
-
-
-
-    # LATER
-    @staticmethod # broken, take a look
-    def generate_search_urls_and_extra_info():
-        # initialize
-        search_urls = []
-        extras = []
-
-
-        # dynamic query parameters
-        hull_materials = {'aluminium': 100, 'composite': 101, 'fiberglass': 102,
-                          'steel': 103, 'wood': 104, 'other': 105, 'hypalon': 106,
-                          'pvc': 107, 'ferro-cement': 108, 'carbon-fiber': 110}
-        fuels = {'gas': 100, 'diesel': 101, 'other': 102}
-        number_of_engines = {1: 100, 2: 101, 'other': 102, 'none': 103}
-        is_new = [True, False]
-        within_x_days = {1: 1535580789155, 3: 1535407989155, 7: 1535062389155, 14: 1534457589155, 30: 1533075189155,
-                    60: 1530483189155}
-        recent_day = new_or_used = material = fuel = engine_number = 'unknown'
-
-        if daily_search:
-            within_x_days = {1: 1535580789155 }
-
-
-        # generate dynamic queries
-        for recent_day, new_or_used, material, fuel, engine_number in product(
-                within_x_days, is_new, hull_materials,
-                fuels, number_of_engines):
-
-            extra_info = {'days': recent_day, 'is_new': new_or_used,
-                          'material': material, 'fuel': fuel, 'number_of_engines': engine_number}
-
-            detailed_query = {
-                            'is': new_or_used,
-                            'hmid': material,
-                            'ftid': fuel,
-                            'enid': engine_number,
-                            'pbsint': recent_day,
-                            }
-
-            search_url = urlencode(OrderedDict(data=base_url, search=search_query))
-            search_urls.append(search_url)
-            extras.append(extra_info)
-
-        return search_urls, extras
