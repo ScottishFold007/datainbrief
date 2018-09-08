e0397e178098d68cc2c3c4ac536015c4624b1ecb
Selim <ozturkselim00@gmail.com>
2018-09-08 13:22:04 +0300

    Debug successful

--- gemi/spiders/gemi_spider.py
+++ gemi/spiders/gemi_spider.py
@@ -22,29 +22,25 @@ class GemiSpider(scrapy.Spider):
         super(GemiSpider, self).__init__(*args, **kwargs)
         # basics
         self.base_url = 'https://www.yachtworld.com'
-        self.base_query = dict()
-        self.base_search_query_url = 'https://www.yachtworld.com/core/listing/cache/searchResults.jsp'
+        self.base_query_parameters = dict()
+        self.root_search_url = 'https://www.yachtworld.com/core/listing/cache/searchResults.jsp'
 
         # control knobs
         self.next_page = next_page  # follow to the next pages
         self.should_get_details = details  # parse details page
         self.daily_search = daily_search  # search recent day only
 
-        self.start_urls = [
-            "https://www.yachtworld.com/core/listing/cache/searchResults.jsp?fromLength=25&toLength=&fromYear=1995&toYear=&fromPrice=20000&toPrice=8000000&luom=126&currencyid=100&ps=50"]
-
         # after the query is generated, start_requests is called automatically
-        # self.generate_base_query_url()
+        self.start_urls = self.generate_base_query_urls()
 
     def start_requests(self):
         for url in self.start_urls:
-            self.logger.info('%s\n\n\n' % url)
-
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def generate_base_query_url(self):
+    def generate_base_query_urls(self):
+        urls = list()
         # default query
-        self.base_query = {
+        self.base_query_parameters = {
             'fromLength': 25,
             'toLength': '',
             'fromYear': 1995,
@@ -55,30 +51,65 @@ class GemiSpider(scrapy.Spider):
             'currencyid': 100,  # US dollar
             'ps': 50  # entries per page
         }
+
+        within_x_days = {1: 1535580789155, 3: 1535407989155, 7: 1535062389155, 14: 1534457589155, 30: 1533075189155,
+                         60: 1530483189155}
+
+
         if self.daily_search:
-            self.base_query['pbsint'] = 1535580789155  # 1 day
+            self.base_query_parameters['pbsint'] = within_x_days[1]  # 1 day
 
-        base_query_string = urlencode(self.base_query, 'utf-8')
-        base_query_url = self.base_search_query_url + '?' + base_query_string
+        for day in within_x_days:
+            self.base_query_parameters['pbsint'] = day
 
-        self.start_urls = [base_query_url]
+            query_string = urlencode(self.base_query_parameters, 'utf-8')
+            query_url = self.root_search_url + '?' + query_string
+            urls.append(query_url)
+
+        return urls
 
     def parse(self, response):
-        self.logger.info('%s\n\n\n' % self.start_urls)
-
-        self.logger.info('%s\n\n\n' % response)
-
-        # selectors
+        # table selectors
         search_results_table_selector = 'div#searchResultsDetailsABTest'
         result_count_selector = 'div.searchResultsCount--mobile-container__searchResultsCount'
+        # field selectors
+        link_selector = 'div.make-model a::attr(href)'
+        length_selector = 'div.make-model a span.length::text'
+        price_selector = 'div.price::text'
+        location_selector = 'div.location::text'
+        broker_selector = 'div.broker::text'
 
         # define the data to process
         search_results = response.css(search_results_table_selector)
         # result_count = response.css(result_count_selector).extract()
 
         for page in search_results:
-            lengths, links, prices, locations, brokers = self.extract_ads(page)
-            self.process_items(lengths, links, prices, locations, brokers, page)
+            # parse fields
+            lengths = page.css(length_selector).extract()
+            links = page.css(link_selector).extract()
+            prices = page.css(price_selector).extract()
+            locations = page.css(location_selector).extract()
+            brokers = page.css(broker_selector).extract()
+
+            # remove empty prices
+            prices = GemiUtil.remove_empty_prices(prices)
+
+
+            for length, link, price, location, broker in zip(lengths, links, prices, locations, brokers):
+
+                link_to_the_item_details, basic_fields = self.get_basic_fields(length, link, price, location, broker)
+
+                # go to the item page to get details
+                if self.should_get_details:
+                    # send a request to the details page
+                    yield scrapy.Request(
+                        link_to_the_item_details,
+                        callback=self.parse_details(page=page),
+                        meta=basic_fields
+                    )
+                else:
+                    # send the item to the pipeline
+                    yield basic_fields
 
         # follow to the next page
         if self.next_page:
@@ -86,63 +117,30 @@ class GemiSpider(scrapy.Spider):
             if next_button is not None:
                 yield response.follow(next_button, callback=self.parse)
 
-    @staticmethod
-    def extract_ads(page):
-        # selectors
-        link_selector = 'div.make-model a::attr(href)'
-        length_selector = 'div.make-model a span.length::text'
-        price_selector = 'div.price::text'
-        location_selector = 'div.location::text'
-        broker_selector = 'div.broker::text'
+    def get_basic_fields(self, length, link, price, location, broker):
 
-        # parse fields
-        lengths = page.css(length_selector).extract()
-        links = page.css(link_selector).extract()
-        prices = page.css(price_selector).extract()
-        locations = page.css(location_selector).extract()
-        brokers = page.css(broker_selector).extract()
+        # get the year and model from the link
+        split_link = link.split('/')
+        year, model = split_link[2], split_link[3]
 
-        # remove empty prices
-        prices = GemiUtil.remove_empty_prices(prices)
+        # clean the fields
+        cleaned_fields = list(map(lambda field: " ".join(field.split()), [price, length, location, broker]))
+        price, length, location, broker = cleaned_fields
 
-        return lengths, links, prices, locations, brokers
+        # make the link work
+        link_to_the_item_details = self.base_url + link
 
-    def process_items(self, lengths, links, prices, locations, brokers, page):
-        # iterate through items
-        for length, link, price, location, broker in zip(lengths, links, prices, locations, brokers):
+        basic_fields = {
+            'model': model,
+            'year': year,
+            'length': length,
+            'price': price,
+            'location': location,
+            'broker': broker,
+            'link': link_to_the_item_details
+        }
 
-            # get the year and model from the link
-            split_link = link.split('/')
-            year, model = split_link[2], split_link[3]
-
-            # clean the fields
-            cleaned_fields = list(map(lambda field: " ".join(field.split()), [price, length, location, broker]))
-            price, length, location, broker = cleaned_fields
-
-            # make the link clickable
-            link = self.base_url + link
-
-            basic_fields = {
-                'model': model,
-                'year': year,
-                'length': length,
-                'price': price,
-                'location': location,
-                'broker': broker,
-                'link': link
-            }
-
-            # go to the item page to get details
-            if self.should_get_details:
-                # send a request to the details page
-                yield scrapy.Request(
-                    link,
-                    callback=self.parse_details(page=page),
-                    meta=basic_fields
-                )
-            else:
-                # send the item to the pipeline
-                yield basic_fields
+        return link_to_the_item_details, basic_fields
 
     @staticmethod
     def parse_details(response, page):
@@ -178,6 +176,7 @@ class GemiSpider(scrapy.Spider):
         for recent_day, new_or_used, material, fuel, engine_number in product(
                 within_x_days, is_new, hull_materials,
                 fuels, number_of_engines):
+
             extra_query_parameters = {'is': new_or_used,
                                       'hmid': material,
                                       'ftid': fuel,
