7e7cb63fb26b1083f0e7e2c150a9479e5fe8b650
Selim <ozturkselim00@gmail.com>
2018-09-18 18:33:19 +0300

    Create link index

--- gemi/database.py
+++ gemi/database.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient, IndexModel, ASCENDING, DESCENDING, TEXT
+from pymongo import MongoClient, IndexModel, ASCENDING, DESCENDING, TEXT, HASHED
 
 
 def get_db_client():
@@ -46,22 +46,22 @@ def rename_field(oldname, newname):
     print('renamed %s to %s' % (oldname, newname))
 
 
-def create_index():
-    db.yachts.create_index([('link', TEXT)], unique=True)  # prevent duplicate ads next time
-    print('created index')
+def create_index(index_name, is_unique):
+    db.yachts.create_index([(index_name, ASCENDING)], unique=is_unique)  # prevent duplicate ads next time
+    print('created %s' % index_name)
 
 
-def drop_index():
-    db.yachts.drop_index([('link', TEXT)])
-    print('dropped index')
+def drop_index(index_name):
+    db.yachts.drop_index([(index_name, TEXT)])
+    print('dropped %s' % index_name)
 
 
 def get_index_info():
     print(db.yachts.index_information())
 
 
-def remove_based_on_existence():
-    db.yachts.delete_many({"year": {"$exists": False}})
+def remove_doc_based_on_existence_of_a_field(field_name):
+    db.yachts.delete_many({field_name: {"$exists": False}})
 
 
 def remove_field(field_name):
@@ -75,3 +75,5 @@ def remove_field(field_name):
 
 
 if __name__ == "__main__":
+    create_index('link', is_unique=True)
+    get_index_info()
