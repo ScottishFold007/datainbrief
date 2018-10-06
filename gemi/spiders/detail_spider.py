9defd98cd7f475bb430db635db995b90cd23f3a3
Selim <ozturkselim00@gmail.com>
2018-10-06 18:48:52 +0300

    Remove pipelines.py

--- gemi/spiders/detail_spider.py
+++ gemi/spiders/detail_spider.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # packages
 import scrapy
-from gemi.pipelines import DetailPipeline
+from gemi.database import get_client_and_db
 
 
 class DetailSpider(scrapy.Spider):
@@ -12,7 +12,8 @@ class DetailSpider(scrapy.Spider):
     # entry point
     def __init__(self, *args, **kwargs):
         super(DetailSpider, self).__init__(*args, **kwargs)
-        self.start_urls = DetailPipeline.get_links()
+        client, db = get_client_and_db()
+        self.start_urls = db.yachts.distinct('link')
 
     # Send urls to parse
     def start_requests(self):
@@ -23,17 +24,17 @@ class DetailSpider(scrapy.Spider):
         full_spec_selector = 'div.full_specs div:first-child::text'
         full_specs = response.css(full_spec_selector).extract()
 
-        hour_in_various_languages = {'hour', 'time', 'stunde', 'ora', 'heure', 'uur', 'tunnin', 'timme', 'saat',
-                                     'hora'}
-        hours = list()
+        # hour_in_various_languages = {'hour', 'time', 'stunde', 'ora', 'heure', 'uur', 'tunnin', 'timme', 'saat',   'hora'}
+
+        specs = dict()
+        self.logger.info(full_specs)
+
         for line in full_specs:
             line = " ".join(line.split()).lower()
-            if any(word in line for word in hour_in_various_languages):
-                hours.append(line)
+            line = line.split(':')
+            self.logger.info(line)
+            spec_key = line[0]
+            spec_value = line[1]
+            specs[spec_key] = spec_value
 
-        # link = response.meta['url']
-
-        item = {'hours':hours}
-
-        # send the item info to the pipeline
-        yield item
+        yield specs
