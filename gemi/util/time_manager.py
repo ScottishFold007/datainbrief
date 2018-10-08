3141458c1b4fe19c6bb6f51112d9e9de76f746fc
Selim <ozturkselim00@gmail.com>
2018-10-08 20:52:19 +0300

    Simplify time

--- gemi/util/time_manager.py
+++ gemi/util/time_manager.py
@@ -3,11 +3,9 @@ from datetime import datetime
 date_now = datetime.now().date().isoformat()
 
 
-class TimeManager:
-    @staticmethod
-    def get_date_of_x_days_ago(days_ago):
-        return datetime.now().date() - datetime.timedelta(days=days_ago)
+def get_date_of_x_days_ago(days_ago):
+    return datetime.now().date() - datetime.timedelta(days=days_ago)
 
-    @staticmethod
-    def str_to_date(str):
-        return datetime.strptime(str, '%Y-%m-%d').date()
+
+def str_to_date(str):
+    return datetime.strptime(str, '%Y-%m-%d').date()
