c4b68ad5f643bf998853c9a5fe3617119a47e98b
Selim <ozturkselim00@gmail.com>
2018-10-10 23:50:20 +0300

    Modularize updating

--- gemi/pipelines/item_updater.py
+++ gemi/pipelines/item_updater.py
@@ -13,7 +13,6 @@ class ItemUpdater(object):
 
     @classmethod
     def update_already_existing_item(cls, link, price, sale_pending):
-        updates = dict()
 
         item = db[collection_name].find_one({"link": link})
 
@@ -24,11 +23,26 @@ class ItemUpdater(object):
         if cls.is_already_updated(item):
             return True
 
-        # check the price
-        last_price = item['price']
+        price_updates = ItemUpdater.check_price_changes(item, price)
+        sale_updates = ItemUpdater.check_sale_status(sale_pending)
+        # merge dicts
+        updates = {**price_updates, **sale_updates}
 
+        print('updated: ', updates)
+
+        return updates
+
+    @staticmethod
+    def check_sale_status(sale_pending):
+        if sale_pending:
+            return {'status.sale_pending': True,
+                    'dates.sale_pending': date_now}
+
+    @staticmethod
+    def check_price_changes(item, price):
+        last_price = item['price']
         if last_price != price:
-            updates = {
+            price_updates = {
                 'price': Cleaner.clean_price(price),
                 'updated': True,
                 'dates.price_changed': date_now,
@@ -40,18 +54,12 @@ class ItemUpdater(object):
 
             try:
                 old_prices = item['old_prices']
-                updates['old_prices'] = old_prices.append(last_price)
+                price_updates['old_prices'] = old_prices.append(last_price)
             except KeyError:
-                updates['old_prices'] = [last_price]
-
-        # check sale status
-        if sale_pending:
-            updates['status.sale_pending'] = True
-            updates['dates.sale_pending'] = date_now
-
-        print('updated: ', updates)
-
-        return updates
+                price_updates['old_prices'] = [last_price]
+        else:
+            price_updates = {}
+        return price_updates
 
     @staticmethod
     def save_updated_item(link, updates):
