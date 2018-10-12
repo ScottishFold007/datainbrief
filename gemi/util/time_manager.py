080ab6a2ae97d7978e24421efd8e1460ab2772e6
Selim <ozturkselim00@gmail.com>
2018-10-13 02:15:10 +0300

    Remove timedelta

--- gemi/util/time_manager.py
+++ gemi/util/time_manager.py
@@ -3,12 +3,5 @@ from datetime import datetime
 date_now = datetime.now().date().isoformat()
 
 
-def get_date_of_x_days_ago(days_ago):
-    return datetime.now().date() - datetime.timedelta(days=days_ago)
-
-
-date_yesterday = get_date_of_x_days_ago(1).isoformat()
-
-
 def str_to_date(str):
     return datetime.strptime(str, '%Y-%m-%d').date()
